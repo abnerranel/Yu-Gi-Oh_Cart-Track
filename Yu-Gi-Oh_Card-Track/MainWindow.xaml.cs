2c2d43b468e581565092579c601cdd8b52438a48
Abner Ranel <abner.ranel@outlook.com>
2024-09-08 21:34:51 -0300

    Add project files.

---
+++ Yu-Gi-Oh_Card-Track/MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace Card_Library_FM_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string currentDir = Environment.CurrentDirectory;
+        private Dictionary<int, CardData> cards = new Dictionary<int, CardData>();
+        OpenFileDialog fileDialog = new OpenFileDialog();
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        string memCardPath;
+        Dispatcher dispatcher;
+        bool processing = false;
+        int totalCards = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadImages();
+            this.Cards.ItemsSource = cards;
+            fileDialog.FileOk += FileDialog_FileOk;
+            dispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        public void LoadImages()
+        {
+            foreach (string file in Directory.EnumerateFiles($"{currentDir}/icons", "*.PNG"))
+            {
+                FileInfo fileinfo = new FileInfo(file);
+                int id = 0;
+                int.TryParse(fileinfo.Name.Replace(fileinfo.Extension, ""), out id);
+                cards.Add(id, new CardData
+                {
+                    ImageData = fileinfo.FullName,
+                    Opacity = 1,
+                });
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            fileDialog.ShowDialog();
+        }
+
+        private void FileDialog_FileOk(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (sender == null) return;
+            var file = sender as OpenFileDialog;
+            txtMemCard.Text = Path.GetFileName(file.FileName);
+            memCardPath = file.FileName;
+            watcher.Path = Path.GetDirectoryName(file.FileName);
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                         | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.*";
+            watcher.Changed += OnChanged;
+            watcher.EnableRaisingEvents = true;
+            _ = ReadMCAsync();
+        }
+
+        private async Task ReadMCAsync()
+        {
+            if(memCardPath == null) return;
+            processing = true;
+            await Task.Factory.StartNew(() =>
+            {
+                FileStream fileStream = new FileStream(memCardPath, FileMode.Open, FileAccess.Read, FileShare.Read, 64 * 1024,
+                    (FileOptions)0x20000000 | FileOptions.WriteThrough & FileOptions.SequentialScan);
+                MemoryStream ms = new MemoryStream();
+                fileStream.CopyTo(ms);
+                List<int>? list = ReadMemCard.Read(ms);
+                if (list == null) return;
+                totalCards = list.Count;
+                foreach (var card in list)
+                {
+                    cards[card].Opacity = 0.5;
+                }
+                cards = cards.OrderByDescending(x => x.Value.Opacity).ThenBy(x => x.Key).ToDictionary();
+                dispatcher.Invoke(() =>
+                {
+                    this.Cards.ItemsSource = cards;
+                    this.txtCont.Content = $"{totalCards} / 722";
+                    //this.Cards.Items.Refresh();
+                });
+            });
+            processing = false;
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            if (source == null) return;
+            if (!processing)
+            {
+                _ = ReadMCAsync();
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < cards.Count; i++) {
+                cards[i].Opacity = 1;
+            }
+            cards = cards.OrderBy(x => x.Key).ToDictionary();
+            totalCards = 0;
+            dispatcher.Invoke(() =>
+            {
+                this.Cards.ItemsSource = cards;
+                this.txtCont.Content = $"{totalCards} / 722";
+                //this.Cards.Items.Refresh();
+            });
+            processing = false;
+        }
+    }
+
+    class CardData
+    {
+        public string ImageData { get; set; }
+        public double Opacity { get; set; }
+    }
+}
---
+++ Yu-Gi-Oh_Card-Track/ReadMemCard.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+
+namespace Card_Library_FM_WPF
+{
+    public class ReadMemCard
+    {
+        const int LIBRARY_BYTE_SIZE = 91;
+        const int MEMCARD_BLOCK_SIZE = 0x2000;
+        const int FRAME_OFFSET = 0xCBC;
+        const int OFFSET = MEMCARD_BLOCK_SIZE + FRAME_OFFSET;
+        const int MIN_MEMCARD_SIZE = 128 * 1024;
+        const int LARGE_MEMCARD_SIZE = 256 * 1024;
+
+        static void PrintHelp(string executableName)
+        {
+            Console.WriteLine($"Usage: {executableName} (options) [memory card image] [output file]");
+            Console.WriteLine("\nOptions:\n--help\t\tPrints this help text\n");
+            Console.WriteLine("--block [n]\tRead the nth block on the memory card. Default: 1. Values other than 1 are");
+        }
+
+        public static List<int>? Read(MemoryStream file)
+        {
+            int offset = OFFSET;
+
+            byte[] buf = new byte[LIBRARY_BYTE_SIZE];
+
+
+            file.Seek(0, SeekOrigin.End);
+            int size = (int)file.Position;
+            if (size < MIN_MEMCARD_SIZE)
+            {
+                Console.WriteLine("Memory card image is too small. Memory cards must be at least 128kb.");
+                return null;
+            }
+            if (size != MIN_MEMCARD_SIZE && size != LARGE_MEMCARD_SIZE)
+            {
+                Console.WriteLine("Memory card image size is not an expected value. Memory cards should be 128kb or 256kb. Results may be inaccurate.\n");
+            }
+            file.Seek(offset, SeekOrigin.Begin);
+            int countRead = file.Read(buf, 0, LIBRARY_BYTE_SIZE);
+            if (countRead != LIBRARY_BYTE_SIZE)
+            {
+                Console.WriteLine("Unexpected number of elements in file stream. Results may be inaccurate.\n");
+            }
+
+
+            var cards = new List<int>();
+            int card = 1;
+            int total = 0;
+            for (uint index = 0; index < LIBRARY_BYTE_SIZE; index++)
+            {
+                byte bitfield = buf[index];
+                for (uint bit = 1; bit <= 8; bit++)
+                {
+                    if (card == 1) // the top bits of the 1st library byte are ignored
+                    {
+                        bit++;
+                    }
+                    byte currentBit;
+                    Byte.TryParse((8 - bit).ToString(), out currentBit);
+                    if ((bitfield >> currentBit & 0x01) != 0)
+                    {
+                        cards.Add(card);
+                        total++;
+                    }
+                    card++;
+                }
+            }
+            Console.WriteLine($"Total cards: {total}");
+            return cards;
+        }
+    }
+}
